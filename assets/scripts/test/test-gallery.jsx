e9447519bbd5e3310a39f00b0f5bb8fae7f75ae4
Andrea Cardy <acardy@customink.com>
2016-09-09 08:38:26 -0400

    Added loop for test.

--- assets/scripts/test/test-gallery.jsx
+++ assets/scripts/test/test-gallery.jsx
@@ -12,8 +12,8 @@ describe('Gallery component', function() {
             'description': 'This is a cat.'
           },
           {
-            'url': 'http://www.bharatint.com/img/categories/our-cat-shop-image.png',
-            'description': 'This is another cat.'
+            'url': 'https://www.cesarsway.com/sites/newcesarsway/files/styles/large_article_preview/public/Common-dog-behaviors-explained.jpg?itok=FSzwbBoi',
+            'description': 'This is a dog.'
           }
         ];
         var renderer = TestUtils.createRenderer();
@@ -22,12 +22,10 @@ describe('Gallery component', function() {
         result.props.className.should.equal('gallery');
         result.props.children.length.should.equal(2);
 
-        var galleryImage1 = result.props.children[0];
-        var galleryImage2 = result.props.children[1];
-    
-        galleryImage1.props.url.should.equal(testImages[0].url);
-        galleryImage1.props.description.should.equal(testImages[0].description);
-        galleryImage2.props.url.should.equal(testImages[1].url);
-        galleryImage2.props.description.should.equal(testImages[1].description);
+        Object.keys(result.props.children).map(function(id, index) {
+            var galleryImage = result.props.children[index];
+            galleryImage.props.url.should.equal(testImages[index].url);
+            galleryImage.props.description.should.equal(testImages[index].description);
+        });
     });
 });
